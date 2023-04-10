c50a2cb8248c527a7940117ee54c763fb57cb01c
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2023-04-10 19:16:58 +0200

    DataPage interface creation

---
+++ portafolio/src/app/interfaces/data-page.interface.ts
@@ -0,0 +1,10 @@
+export interface DataPage {
+    title?: string;
+    email?: string;
+    name?: string;
+    author_git_page?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    work_team?: any[];
+}
--- portafolio/src/app/services/info-page.service.ts
+++ portafolio/src/app/services/info-page.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DataPage } from '../interfaces/data-page.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPageService {
 
-  info: any = {};
+  info: DataPage = {};
   infoLoaded: boolean = false;
 
   constructor(private http: HttpClient) { 
     console.log('InfoPageService - LOADED');
 
-    this.http.get('assets/data/data-page.json').subscribe(resp => {
+    this.http.get('assets/data/data-page.json').subscribe((resp: DataPage) => {
       this.infoLoaded = true;
       this.info = resp;
       console.log(resp);
